3b47ce70890b20b70b84d42b39ddff4a735952fe
Damian Kulp <damiankulp@gmail.com>
2012-05-19 19:19:27 -0400

    fixed sign in (is now possible), introduced bug: login fail shows on page load.  Show hide on username and password fields now works.

--- src/hello.php
+++ src/hello.php
@@ -1,3 +1,5 @@
-<?php
+<?php 
+    include("include/header.php");
+    session_start();
     echo "<script type='text/javascript' src='../javascript/made_it.js'></script>";
 ?>
--- src/include/header.php
+++ src/include/header.php
@@ -3,13 +3,15 @@
     $sign_in = -1;
     if( !isset($_POST["username"]) && !isset($_POST["password"]) ){
         session_start();
+        $the = 0;
     }
     else if( isset($_POST["username"]) && isset($_POST["password"]) ){    
+        $the = 1;
         try {
             $dbc = mysql_connect('localhost', 'k16768_the_max', '4f3318d83020c', 'k16768_the_max');
             if( $dbc == null ) {
                 throw new Exception("Hmmm, something went wrong when trying to submit your info, you should try again now or later to see if it works.<br /> If you are still getting errors you can try contacting us.<br /><br />Error: #" . mysql_errno() . " " . mysql_error() . "<br />");
-            }  
+            }
         }
         catch(Exception $e) {
             echo 'Message: ' . $e->getMessage();
@@ -18,26 +20,29 @@
         if( $database == 1 ) {
             $query = "SELECT username, password FROM k16768_the_max.users WHERE username = '" . $_POST['username'] . "'";
             $result = mysql_query($query);
-            mysql_close($dbc);
-            session_start();
             $row = mysql_fetch_array($result, MYSQL_ASSOC);
-            if( $row['username'] == $_POST['username'] && $row['password'] == $_POST['password'] ) {
+            if( $row['password'] == md5($_POST['password']) ) {
+                session_start();
                 $_SESSION['username'] = $_POST['username'];
                 $_SESSION['password'] = $_POST['password'];
-                $sign_in = 1;
+                $the = 2;
             }
             else { 
                 $sign_in = 0;
+                $the = 3;
             }
         }
+        mysql_close($dbc);
     }
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <?php
-            if( $sign_in == 0 ) {
+        <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+        <script type='text/javascript' src='../javascript/page.php'></script>
+<?php
+            if( !isset($_SESSION['username']) ) {
                 echo "<script type='text/javascript' src='../javascript/error.js'></script>";
             }
         ?>
@@ -53,18 +58,18 @@
                 <div id="top_bar">
                     
                     <?php
-                        if($sign_in == 1) {
+                        if( isset($_SESSION['username']) ) {
                             echo "<a href='sign_out.php'>sign out</a>";
-                            echo $_SESSION['username'];
                         }
                         else {
                             echo "<a id='sign'>sign in</a>";
                         }
                     ?>
+                    
                     <a href="new_account.php">sign up</a>
                 </div>
                 <div id="head1">
-                    <form id="user_pass" target="index.php" method="post">
+                    <form id="user_pass" action="hello.php" method="post">
                         <input id="input_user" type="text" placeholder="username" name="username" />
                         <input type="password" name="password" placeholder="password" />
                         <input type="submit" name="submit" />
@@ -103,6 +108,11 @@
                         </li>
                     </ul>
                 </div>
+                <?php echo $the;?>
+                <br />
+                
+            
+                
 
 
 
